33d0ad11940d55db7f36714720375965684fa7f9
TahmidU <tahmiduddin@hotmail.co.uk>
2019-04-18 01:07:59 +0100

    Collision with food detected. Food relocates when eaten. Snake segment growth needs work(probably needs a double linked list to move from tail to head).

--- snake_game.py
+++ snake_game.py
@@ -1,9 +1,15 @@
 import pygame
 import random
 
+
+
+
 class snake:
 
     class node:
+
+        prev_x = 0
+        prev_y = 0
 
         def __init__(self, x, y, next=None):
             self.x = x
@@ -11,24 +17,25 @@ class snake:
             self.next = next
 
     head = None
+    tail = None
 
     snake_color = 255,0,0
     snake_width = 10
     snake_height = 10
 
     def isEmpty(self):
-        return self.head == None
+        return self.tail == None
 
-    def addToHead(self, x, y, next):
+    def addToTail(self, x, y):
         if self.isEmpty():
-            print("Is Empty")
-            self.head = self.node(x, y)
+            self.tail = self.node(x, y)
+            self.head = self.tail
         else:
-            print("Not Empty")
-            self.head = self.node(x, y, next)
+            self.tail.next = self.node(x, y)
+            self.tail = self.tail.next
 
     def __init__(self, x, y):
-        self.addToHead(x, y, None)
+        self.addToTail(x, y)
 
 class food:
     food_colour = 0,255,0
@@ -36,63 +43,130 @@ class food:
     food_height = 10
 
     def __init__(self):
-        self.x = (random.randint(1,160)*self.food_width)%799   
-        self.y = (random.randint(1, 80)*self.food_height)%399
+        self.x = random.randint(0,game.width-1)
+        self.x = self.x - (self.x % food.food_width)
+        self.y = random.randint(0, game.height-1)
+        self.y = self.y - (self.y % food.food_height)
+        
+class game:
+
+    size = width, height = 800,400
+
+    def __init__(self):
+        pygame.init()
+
+        #Simple game properties
+        pygame.display.set_caption("snake")
+        background_colour = 0,0,0
+        clock = pygame.time.Clock()
+        screen = pygame.display.set_mode(game.size)
+        
+        #Create snake and food
+        player = snake(game.width/2, game.height/2)
+        pressed = None #Nothing is pressed at the beginning
+        apple = food()
+
+        run = True
+        while run:
+            clock.tick(15)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+            #Draw background
+            screen.fill(background_colour)
+            
+            #Keys pressed events
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_a]:
+                if pressed != pygame.K_d:
+                    pressed = pygame.K_a
+            if keys[pygame.K_d]:
+                if pressed != pygame.K_a:
+                    pressed = pygame.K_d
+            if keys[pygame.K_w]:
+                if pressed != pygame.K_s:
+                    pressed = pygame.K_w
+                pressed = pygame.K_w
+            if keys[pygame.K_s]:
+                if pressed != pygame.K_w:
+                    pressed = pygame.K_s
+
+             
+
+                 
+            #Player movement
+            if pressed == pygame.K_a:
+                aux = player.head #Pointer for linked list
+                aux.prev_x = aux.x
+                aux.prev_y = aux.y
+                while(aux != None):
+                    if aux.next != None:
+                        aux.next.x = aux.prev_x
+                        aux.next.y = aux.prev_y
+                        aux.next.prev_x = aux.next.x
+                        aux.next.prev_y = aux.next.y
+                    aux = aux.next    
+                player.head.x = player.head.x - player.snake_width
+            if pressed == pygame.K_d:
+                aux = player.head #Pointer for linked list
+                aux.prev_x = aux.x
+                aux.prev_y = aux.y
+                while(aux != None):
+                    if aux.next != None:
+                        aux.next.x = aux.prev_x
+                        aux.next.y = aux.prev_y
+                        aux.next.prev_x = aux.next.x
+                        aux.next.prev_y = aux.next.y
+                    aux = aux.next
+                player.head.x = player.head.x + player.snake_width
+            if pressed == pygame.K_w:
+                aux = player.head #Pointer for linked list
+                aux.prev_x = aux.x
+                aux.prev_y = aux.y
+                while(aux != None):
+                    if aux.next != None:
+                        aux.next.x = aux.prev_x
+                        aux.next.y = aux.prev_y
+                        aux.next.prev_x = aux.next.x
+                        aux.next.prev_y = aux.next.y
+                    aux = aux.next
+                player.head.y = player.head.y - player.snake_height
+            if pressed == pygame.K_s:
+                aux = player.head #Pointer for linked list
+                aux.prev_x = aux.x
+                aux.prev_y = aux.y
+                while(aux != None):
+                    if aux.next != None:
+                        aux.next.x = aux.prev_x
+                        aux.next.y = aux.prev_y
+                        aux.next.prev_x = aux.next.x
+                        aux.next.prev_y = aux.next.y
+                    aux = aux.next
+                player.head.y = player.head.y + player.snake_height
+            
+            if player.head.x == apple.x and player.head.y == apple.y:
+                player.addToTail(player.tail.x, player.tail.y)
+                apple = food()
+
+            aux = player.head
+            count = 0
+            while(aux != None):
+                count = count + 1
+                print("This is " + str(count) + " and its x pos is: " + str(aux.x))
+                aux = aux.next
+            #print(count)    
+            
+            #draw food
+            pygame.draw.rect(screen, apple.food_colour, (apple.x, apple.y, apple.food_width, apple.food_height))
+
+            aux = player.head #Pointer for linked list
+            while(aux != None):
+                pygame.draw.rect(screen, player.snake_color, (aux.x, aux.y, player.snake_width, player.snake_height))
+                aux = aux.next
+
+            pygame.display.flip() 
+
 
 if __name__ == "__main__":
-    pygame.init()
-
-    pygame.display.set_caption("snake")
-    size = width, height = 800, 400
-    screen = pygame.display.set_mode(size)
-
-    background_colour = 0,0,0
-
-    player = snake(width/2, height/2)
-    pressed = None
-
-    apple = food()
-
-    run = True
-    while run:
-        pygame.time.delay(100)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-
-        screen.fill(background_colour)
-        pygame.draw.rect(screen, apple.food_colour, (apple.x, apple.y, apple.food_width, apple.food_height))
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
-            if pressed != pygame.K_d:
-                pressed = pygame.K_a
-        if keys[pygame.K_d]:
-            if pressed != pygame.K_a:
-                pressed = pygame.K_d
-        if keys[pygame.K_w]:
-            if pressed != pygame.K_s:
-                pressed = pygame.K_w
-            pressed = pygame.K_w
-        if keys[pygame.K_s]:
-            if pressed != pygame.K_w:
-                pressed = pygame.K_s
-
-        aux = player.head
-        while(aux != None):
-            if pressed == pygame.K_a:
-                aux.x = aux.x - player.snake_width
-            if pressed == pygame.K_d:
-                aux.x = aux.x + player.snake_width
-            if pressed == pygame.K_w:
-                aux.y = aux.y - player.snake_height
-            if pressed == pygame.K_s:
-                aux.y = aux.y + player.snake_height
-            pygame.draw.rect(screen, player.snake_color, (aux.x, aux.y, player.snake_width, player.snake_height))    
-            aux = aux.next    
-        
-        
-        pygame.display.flip()
-        
-
-    pygame.quit()    
+    game()
