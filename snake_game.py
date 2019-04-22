e9ce0f779febfd22cb0289eecbaa667f5e6eed91
TahmidU <tahmiduddin@hotmail.co.uk>
2019-04-23 00:42:05 +0100

    Code refactor a little. Game states added: running and over. Snake can now detect its tail. When the Snake head eats the tail its game over.

--- snake_game.py
+++ snake_game.py
@@ -45,7 +45,6 @@ class snake:
         while(self.aux is not None):
             self.count = self.count + 1
             self.aux = self.aux.next
-        print(self.count)
         return self.count
 
     def __init__(self, x, y):
@@ -60,6 +59,9 @@ class food:
     food_height = 10
 
     def __init__(self):
+        self.spawn()
+
+    def spawn(self):
         self.x = random.randint(0,game.width-1)
         self.x = self.x - (self.x % food.food_width)
         self.y = random.randint(0, game.height-1)
@@ -67,8 +69,20 @@ class food:
 
 class game:
 
-    #game properties.
+    #Game properties.
     size = width, height = 800, 400
+    screen = pygame.display.set_mode(size)
+
+    #States
+    RUNNING, OVER = 0,1
+    state = RUNNING
+
+    def display_msg(self, text, text_size, text_color, x, y):
+            self.font = pygame.font.SysFont(None, text_size)
+            self.text = self.font.render(text, True, text_color)
+            self.textRect = self.text.get_rect()
+            self.textRect.center = (x, y)
+            game.screen.blit(self.text, self.textRect)
 
     def __init__(self):
         pygame.init() #get neccessary modules.
@@ -77,86 +91,104 @@ class game:
         pygame.display.set_caption("snake")
         background_colour = 0,0,0
         clock = pygame.time.Clock()
-        screen = pygame.display.set_mode(game.size)
 
         #Create snake and food
         player = snake(game.width/2, game.height/2)
         pressed = None #Nothing is pressed at the beginning
         apple = food()
 
-        run = True
-        while run:
-            clock.tick(15)
-            for event in pygame.event.get():
-                if(event.type == pygame.QUIT):
-                    run = False
+        if game.state == game.RUNNING:
+            run = True
+            while run:
+                clock.tick(24)
+                for event in pygame.event.get():
+                    if(event.type == pygame.QUIT):
+                        run = False
 
-            #Draw background
-            screen.fill(background_colour)
+                #Draw background
+                game.screen.fill(background_colour)
 
-            #Keys pressed events
-            keys = pygame.key.get_pressed()
-            if(keys[pygame.K_a]):
-                if(pressed != pygame.K_d):
-                    pressed = pygame.K_a
-            if(keys[pygame.K_d]):
-                if(pressed != pygame.K_a):
-                    pressed = pygame.K_d
-            if(keys[pygame.K_w]):
-                if(pressed != pygame.K_s):
-                    pressed = pygame.K_w
-            if(keys[pygame.K_s]):
-                if(pressed != pygame.K_w):
-                    pressed = pygame.K_s
+                #Game Over
+                aux = player.head
+                while(aux.next is not None):
+                    if(player.tail.x == aux.x and player.tail.y == aux.y):
+                        game.state = game.OVER
+                        run = False
+                    aux = aux.next
 
-            #Apple with apple -> add a new add to the single linked list and respawn food.
-            if(player.tail.x == apple.x and player.tail.y == apple.y):
-                player.addToHead(player.head.x, player.head.y)
-                apple = food()
+                #Snake eats apple -> add a new add to the single linked list and respawn food.
+                if(player.tail.x == apple.x and player.tail.y == apple.y):
+                    player.addToHead(player.head.x, player.head.y)
+                    apple.spawn()
 
-            #1st segment of tail follows the head of the snake.
-            if (player.head is not None):
-                player.head.x = player.tail.x
-                player.head.y = player.tail.y
+                #1st segment of tail follows the head of the snake.
+                if (player.head is not None):
+                    player.head.x = player.tail.x
+                    player.head.y = player.tail.y
 
-            #All other segments of tail follow the first segment of the tail. ^
-            aux = player.head
-            while(aux.next is not None):
-                aux.x = aux.next.x
-                aux.y = aux.next.y
-                aux = aux.next
+                #All other segments of tail follow the first segment of the tail. ^
+                aux = player.head
+                while(aux.next is not None):
+                    aux.x = aux.next.x
+                    aux.y = aux.next.y
+                    aux = aux.next
 
-            #Player movement
-            if(pressed == pygame.K_a):
-                player.tail.x = player.tail.x - player.snake_width
-            if(pressed == pygame.K_d):
-                player.tail.x = player.tail.x + player.snake_width
-            if(pressed == pygame.K_w):
-                player.tail.y = player.tail.y - player.snake_height
-            if(pressed == pygame.K_s):
-                player.tail.y = player.tail.y + player.snake_height
+                #Keys pressed events
+                keys = pygame.key.get_pressed()
+                if(keys[pygame.K_a]):
+                    if(pressed != pygame.K_d):
+                        pressed = pygame.K_a
+                if(keys[pygame.K_d]):
+                    if(pressed != pygame.K_a):
+                        pressed = pygame.K_d
+                if(keys[pygame.K_w]):
+                    if(pressed != pygame.K_s):
+                        pressed = pygame.K_w
+                if(keys[pygame.K_s]):
+                    if(pressed != pygame.K_w):
+                        pressed = pygame.K_s
 
-            #Game boarder controls
-            if(player.tail.x < 0):
-                player.tail.x = player.tail.x + game.width
-            if(player.tail.x > game.width):
-                player.tail.x = -player.tail.x
-            if(player.tail.y > game.height):
-                player.tail.y = -player.tail.y
-            if(player.tail.y < 0):
-                player.tail.y = player.tail.y + game.height
+                #Player movement
+                if(pressed == pygame.K_a):
+                    player.tail.x = player.tail.x - player.snake_width
+                if(pressed == pygame.K_d):
+                    player.tail.x = player.tail.x + player.snake_width
+                if(pressed == pygame.K_w):
+                    player.tail.y = player.tail.y - player.snake_height
+                if(pressed == pygame.K_s):
+                    player.tail.y = player.tail.y + player.snake_height
 
-            #draw food
-            pygame.draw.rect(screen, apple.food_colour, (apple.x, apple.y, apple.food_width, apple.food_height))
+                #Game border controls
+                if(player.tail.x < 0):
+                    player.tail.x = player.tail.x + game.width
+                if(player.tail.x > game.width-1):
+                    player.tail.x = -player.tail.x
+                if(player.tail.y > game.height-1):
+                    player.tail.y = -player.tail.y
+                if(player.tail.y < 0):
+                    player.tail.y = player.tail.y + game.height
 
-            #draw snake and all tail segments
-            aux = player.head
-            while(aux is not None):
-                pygame.draw.rect(screen, player.snake_color, (aux.x, aux.y, player.snake_width, player.snake_height))
-                aux = aux.next
+                #Draw food
+                pygame.draw.rect(game.screen, apple.food_colour, (apple.x, apple.y, apple.food_width, apple.food_height))
 
-            #Update is faster than flip(). Doesnt redraw the entire screen.
-            pygame.display.update()
+                #Draw snake and all tail segments
+                aux = player.head
+                while(aux is not None):
+                    pygame.draw.rect(game.screen, player.snake_color, (aux.x, aux.y, player.snake_width, player.snake_height))
+                    aux = aux.next
+
+                #Update is faster than flip(). Doesnt redraw the entire screen.
+                pygame.display.update()
+
+        if game.state == game.OVER:
+                self.display_msg("GAME OVER", 155, (255,255,255), self.width/2, self.height/2)
+                continue_font = 30
+                self.display_msg("press any key to continue...", continue_font, (255,255,255), self.width/2, (self.height/2)+(continue_font*2))
+                pygame.display.update()
+                while True:
+                    for event in pygame.event.get():
+                        if event.type == pygame.KEYDOWN:
+                            return
 
 
 if __name__ == "__main__":
